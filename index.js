de53b204380fbf6ec89a8908649f1afcc9ae9a03
Divyesh Markana <divyeshmarkana724@gmail.com>
2022-02-02 15:49:49 +0530

    create createChart function

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 window.addEventListener("load", createChart);
 window.addEventListener("resize", createChart);
+
+function createChart(e) {
+    
+    const days = document.querySelectorAll(".chart-values li")
+    const tasks = document.querySelectorAll(".chart-bars li")
+
+    const dayArray = [...days]
+
+    tasks.forEach(el => {
+
+        const duration = el.dataset.duration.split("-")
+
+        const startDay = duration[0]
+        const endDay = duration[1]
+
+        let left = 0;
+        let width = 0;
+
+    })
+}
